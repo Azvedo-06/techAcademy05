6521ce3145c729e39f3ad5441d00ed96ca368663
Azvedo-06 <azinho006@gmail.com>
2025-02-26 22:09:12 -0300

    rotas e controller de delete e update

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response} from "express";
 import UserModel from "../models/UserModel";
+import { error } from "console";
+
 
 export const getAllUser = async (req:Request, res:Response) => {
     const usersFindAll = await UserModel.findAll();
@@ -8,16 +10,70 @@ export const getAllUser = async (req:Request, res:Response) => {
 
 export const getUserById = async (req:Request<{id: number}>, res:Response) => {
     const user = await UserModel.findByPk(req.params.id);
+
+    if (!user) {
+        return res.status(400).json({error: "User not found"})
+    }
+
     return res.json(user);
 }
 
 export const createUser = async (req:Request, res:Response) => {
-    //destructing object
-    const { name, email, password, cpf } = req.body;
-    const user = await UserModel.create({name, email, password, cpf});
-    return res.status(201).json(user);
+    try {
+        //destructing object
+        const { name, email, password, cpf } = req.body;
+        
+        if (!name || name === "") {
+            return res.status(400).json({error: "Name is required"});
+        }
+        
+        const user = await UserModel.create({name, email, password, cpf});
+        return res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json('erro interno no servidor:'+ error);
+    }
+}
+   
+export const deleteUser = async (req:Request<{id: number}>, res:Response) => {
+    try {
+        const userDelete = await UserModel.findByPk(req.params.id);
+
+        if (!userDelete) {
+            return res.status(400).json({error: 'User not found'});
+        }
+
+        userDelete.destroy();
+        return res.status(200).json("usuário deletado");
+
+    } catch (error) {
+        res.status(500).json('erro interno no servidor:'+ error);
+    }
 }
 
-//delete
+export const updateUser = async (req:Request<{id:string}>, res:Response) => {
+    try {
+        //destructing object
+        const { name, email, password, cpf } = req.body;
+        
+        if (!name || name === "") {
+            return res.status(400).json({error: "Name is required"});
+        }
 
-//update
+        const user = await UserModel.findByPk(req.params.id);
+
+        if (!user) {
+            return res.status(400).json({error: "User not found"});
+        }
+
+        user.name = name
+        user.email = email
+        user.password = password
+        user.cpf = cpf
+
+        await user.save()
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json('erro interno no servidor:'+ error);
+    }
+}
+
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAllUser, getUserById, createUser } from "../controllers/userController"
+import { getAllUser, getUserById, createUser, updateUser, deleteUser } from "../controllers/userController"
 
 const router = express.Router();
 
 router.get('/users', getAllUser);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
-
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
