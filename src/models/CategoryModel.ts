4ba0ec00c8b354020c1f0cb660791e602e0e6ba4
Azvedo-06 <azinho006@gmail.com>
2025-03-13 17:32:27 -0300

    model de categorias

---
+++ src/models/CategoryModel.ts
@@ -0,0 +1,28 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+
+class CategoryModel extends Model {
+    id: number | undefined;
+    name: string | undefined;
+}
+
+CategoryModel.init(
+    {
+        id: {
+            type: DataTypes.NUMBER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        modelName: 'CategoryModel',
+        tableName: 'categorys'
+    }
+);
+
+export default CategoryModel;
