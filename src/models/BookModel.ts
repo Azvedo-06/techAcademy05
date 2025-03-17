495c9c4aa020e5f220f342027713f2f4b42ab0b6
Azvedo-06 <azinho006@gmail.com>
2025-03-17 16:55:52 -0300

    model de Book( livro )

---
+++ src/models/BookModel.ts
@@ -0,0 +1,70 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+import AuthorModel from "./AuthorModel";
+import CategoryModel from "./CategoryModel";
+
+class BookModel extends Model {
+    id: number | undefined;
+    title: string | undefined;
+    description: string | undefined;
+    publication_date: Date | undefined;
+    authorId: number | undefined; // FK Referência à tabela Autores
+    categoryId: number | undefined; // FK Referência à tabela Categorias
+}
+
+BookModel.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        publication_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        authorId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'BookModel',
+        tableName: 'books'
+    }
+)
+
+BookModel.belongsTo(AuthorModel, {
+    foreignKey: 'authorId', // definindo qual é a fk
+    as: 'author' // define o nome da relação na busca
+})
+// mapeamento bidirecional 
+AuthorModel.hasMany(BookModel, {
+    foreignKey: 'authorId',
+    as: 'author'
+})
+
+BookModel.belongsTo(CategoryModel, {
+    foreignKey: 'categoryId', 
+    as: 'category'
+})
+// mapeamento bidirecional 
+AuthorModel.hasMany(BookModel, {
+    foreignKey: 'categoryId',
+    as: 'category'
+})
+
+export default BookModel;
