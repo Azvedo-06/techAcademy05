5d36f1020375da3cb24c3a5fcfdb58b416875a16
Azvedo-06 <azinho006@gmail.com>
2025-06-11 11:49:50 -0300

    testes login

--- frontend/tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('página inicial carrega corretamente', async ({ page }) => {
-  await page.goto('http://localhost:5173/login');
-  const title = await page.getByText("Bem-vindo de volta!")
-
-  expect(title).toBeTruthy();
-});
---
+++ frontend/tests/login.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Pagina de Login', () => {
+  test('Deve fazer login com sucesso, redireciona para a home(/books)', async ({page}) => {
+    await page.goto('http://localhost:5173/login');
+     
+    await page.fill('#email', 'azinho006@gmail.com');
+    await page.fill('#password', '123456');
+
+    await page.getByRole("button", { name: "Entrar" }).click()
+
+    await expect(page).toHaveURL('http://localhost:5173/');
+  });
+
+  test('Deve falhar ao tentar login com senha incorreta', async ({page}) => {
+    await page.goto('http://localhost:5173/login');
+
+    await page.fill('#email', 'azinho006@gmail.com');
+    await page.fill('#password', '517700');
+
+    await page.getByRole("button", { name: "Entrar" }).click()
+
+    await expect(page.locator('p.text-red-500')).toHaveText(/E-mail ou senha inválidos/);
+  })
+
+  test('deve exibir mensagem de erro ao deixar campos vazios(required)', async ({ page }) => {
+    await page.goto('http://localhost:5173/login');
+
+    await page.getByRole("button", {name: "Entrar"}).click();
+
+    const emailInput = await page.locator('#email');
+    await expect(emailInput).toHaveAttribute('required');
+  });
+})
